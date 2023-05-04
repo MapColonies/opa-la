b06946762505f98dd5008858d0839a9963e4b406
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2023-05-04 13:15:38 +0300

    style(auth-cron): prettier

--- packages/auth-cron/scripts/generateConfigDocs.mjs
+++ packages/auth-cron/scripts/generateConfigDocs.mjs
@@ -1,16 +1,15 @@
 import { readFileSync, writeFileSync } from 'fs';
 import { jsonschema2md } from '@adobe/jsonschema2md';
-import config from '../src/config.js'
+import config from '../src/config.js';
 import path from 'path';
 
-
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
-const res = jsonschema2md(config.configSchema, {includeReadme: true, });
+const res = jsonschema2md(config.configSchema, { includeReadme: true });
 const readme = readFileSync('../README.md', { encoding: 'utf-8' });
 
 // console.log(res.markdown);
 for (const file of res.markdown) {
-  writeFileSync(path.join('files',file.fileName), file.content)
+  writeFileSync(path.join('files', file.fileName), file.content);
 }
 
 // const startIndex = readme.indexOf('<!--- config start --->');
