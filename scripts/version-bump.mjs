3d35c26b133a51fe41cb845a016c6ce3f660afb0
Netanel Cohen <34451523+netanelC@users.noreply.github.com>
2025-01-29 13:49:31 +0200

    feat: add opa to releasing script (#54)

--- scripts/version-bump.mjs
+++ scripts/version-bump.mjs
@@ -15,6 +15,8 @@ const files = [
   { path: 'packages/auth-manager/openapi3.yaml', targets: ['info.version'] },
   { path: 'packages/auth-manager/helm/Chart.yaml', targets: ['version', 'appVersion'] },
   { path: 'packages/auth-cron/helm/Chart.yaml', targets: ['version', 'appVersion'] },
+  { path: 'helm/Chart.yaml', targets: ['version'] },
+  { path: 'helm/charts/opa/Chart.yaml', targets: ['version'] },
 ];
 
 for (const file of files) {
