c93d5264a2074d32ffb1bc9cc1a93bb5a84b16c4
Schnitz <12687466+CptSchnitz@users.noreply.github.com>
2026-04-27 11:13:21 +0300

    chore(global): fixes

--- knip.config.ts
+++ knip.config.ts
@@ -11,7 +11,7 @@ const config: KnipConfig = {
   },
   ignoreBinaries: ['lerna'],
   ignoreFiles: ['example/**'],
-  ignoreDependencies: ['@map-colonies/infra-copilot-instructions'],
+  ignoreDependencies: ['@map-colonies/infra-copilot-instructions', '@vitest/eslint-plugin'],
   workspaces: {
     'packages/auth-core': {
       entry: ['src/config.ts', 'src/db/migrations/*', 'dataSource.{ts,mjs}'],
